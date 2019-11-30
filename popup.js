3c984a648dda7737f83b635bbb5e860c6a52308a
김재구 <rlaworn1993@gmail.com>
2019-11-30 22:01:04 +0900

    (FUNC) now we can update table with data from server

--- popup.js
+++ popup.js
@@ -49,8 +49,9 @@ function GetEachScore(gameTitle, platform) {
     if (xhr.readyState == 1) {
     }
     else if (xhr.readyState == 4) {
-      let score = xhr.response.result.score;
-      DEBUG(`${gameTitle}'s score is ${score}`);
+      let result = xhr.response.result;
+      DEBUG(`${gameTitle}'s score is ${result.score}`);
+      AddDataToTable(result.image,platform,gameTitle,result.score,"13:00")
     }
   }
   var modifiedGameTitle = gameTitle.replace(" ","%20");
@@ -78,5 +79,22 @@ function GetEachScore(gameTitle, platform) {
   xhr.setRequestHeader("x-rapidapi-host", "chicken-coop.p.rapidapi.com");
   xhr.setRequestHeader("x-rapidapi-key", "c976920022msha45b1a7b96d279ap17e7aejsne930cb2ce86d");
   xhr.send(data);
+}
+
+function AddDataToTable(imgURL,platform,title,score,playTime)
+{
+  var table = document.getElementById("resultTable");
+  var row = table.insertRow();
+  var imageURLCell = row.insertCell();
+  var platformCell = row.insertCell();
+  var titleCell = row.insertCell();
+  var scoreCell = row.insertCell();
+  var playTimeCell = row.insertCell();
+
+  imageURLCell.innerHTML = `<img src=${imgURL}>`;
+  platformCell.innerHTML = platform;
+  titleCell.innerHTML = title;
+  scoreCell.innerHTML = score;
+  playTimeCell.innerHTML = playTime;
 }
 
